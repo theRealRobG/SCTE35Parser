dd6365a0430e9be83d801a31214b26c6f6685241
theRealRobG <robertnw5@gmail.com>
2021-02-06 19:44:07 +0000

    Added validate convenience for BitReader

---
+++ Sources/SCTE35Parser/BitParser/DataBitReader.swift
@@ -0,0 +1,33 @@
+//
+//  DataBitReader.swift
+//  
+//
+//  Created by Robert Galluccio on 06/02/2021.
+//
+
+import BitByteData
+
+protocol DataBitReader: BitReader {
+    /// Validate that at least the expected minimum number of bits are left to read from the data.
+    /// - Parameters:
+    ///   - expectedMinimumBitsLeft: The minimum expected bits left to read from the data.
+    ///   - parseDescription: A description of what was being attempted to be parsed that resulted in error.
+    /// - Throws: `ParserError`
+    func validate(expectedMinimumBitsLeft: Int, parseDescription: String) throws
+}
+
+extension DataBitReader {
+    func validate(expectedMinimumBitsLeft: Int, parseDescription: String) throws {
+        if bitsLeft < expectedMinimumBitsLeft {
+            throw ParserError.unexpectedEndOfData(
+                UnexpectedEndOfDataErrorInfo(
+                    expectedMinimumBitsLeft: expectedMinimumBitsLeft,
+                    actualBitsLeft: bitsLeft,
+                    description: parseDescription
+                )
+            )
+        }
+    }
+}
+
+extension MsbBitReader: DataBitReader {}
